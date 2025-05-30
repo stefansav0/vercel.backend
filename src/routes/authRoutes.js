e3ac1625bca462a0b4d2cb0106e1d9ae2730682b
stefansav0 <ravikumar.kumar2333@gmail.com>
2025-05-30 21:10:28 +0530

    Fix: Correct path for authMiddleware import

--- src/routes/authRoutes.js
+++ src/routes/authRoutes.js
@@ -4,8 +4,8 @@ const jwt = require("jsonwebtoken");
 const nodemailer = require("nodemailer");
 const asyncHandler = require("express-async-handler");
 const User = require("../models/User");
+const authMiddleware = require("../middleware/authMiddleware");
 require("dotenv").config();
-const authMiddleware = require("../middlewares/authMiddleware");
 
 const router = express.Router();
 const JWT_SECRET = process.env.JWT_SECRET;
